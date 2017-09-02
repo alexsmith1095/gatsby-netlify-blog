6727af2cd5e4f967668523fea9fd6a6a02931e7e
Alex Smith <alexsmith1095@gmail.com>
2017-09-02 18:08:34 +0100

    added blog plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          `gatsby-remark-images`
+        ] // for any remark specific plugins e.g. gatsby-remark-copy-linked-files, gatsby-remark-prismjs
+      }
+    },
   ],
 }
